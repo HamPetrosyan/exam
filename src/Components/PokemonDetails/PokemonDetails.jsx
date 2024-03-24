c2dcaf808f3b9d169d1d8a4ee0509135f775c0b0
Ham Petrosyan <hampetrosyan007@gmail.com>
2024-03-24 18:01:43 +0400

    updated

--- src/Components/PokemonDetails/PokemonDetails.jsx
+++ src/Components/PokemonDetails/PokemonDetails.jsx
@@ -3,45 +3,44 @@ import { useEffect, useState } from "react";
 const PokemonDetails = ({ pokemonUrl, fetchPokemonDetails }) => {
   const [pokemonDetails, setPokemonDetails] = useState(null);
 
-  const loadPokemonDetails = async () => {
-    if (!pokemonDetails) {
-      const data = await fetchPokemonDetails({ url: pokemonUrl });
-      setPokemonDetails(data);
-    }
-  };
-
   useEffect(() => {
+    const loadPokemonDetails = async () => {
+      if (!pokemonDetails) {
+        const data = await fetchPokemonDetails({ url: pokemonUrl });
+        setPokemonDetails(data);
+      }
+    };
     loadPokemonDetails();
   }, []);
 
-  if (!pokemonDetails) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="pokemon-details">
-      <img
-        src={pokemonDetails.sprites.other.dream_world.front_default}
-        alt="Pokemon"
-      />
-      <div>
-        <h2>Stats</h2>
-        <ul>
-          {pokemonDetails.stats.map((stat, index) => (
-            <li key={index}>
-              {stat.stat.name}: {stat.base_stat}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h2>Types</h2>
-        <ul>
-          {pokemonDetails.types.map((type, index) => (
-            <li key={index}>{type.type.name}</li>
-          ))}
-        </ul>
-      </div>
+      {pokemonDetails && (
+        <>
+          <img
+            src={pokemonDetails.sprites.other.dream_world.front_default}
+            alt="Pokemon"
+          />
+          <div>
+            <h2>Stats</h2>
+            <ul>
+              {pokemonDetails.stats.map((stat, index) => (
+                <li key={index}>
+                  {stat.stat.name}: {stat.base_stat}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h2>Types</h2>
+            <ul>
+              {pokemonDetails.types.map((type, index) => (
+                <li key={index}>{type.type.name}</li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 };
